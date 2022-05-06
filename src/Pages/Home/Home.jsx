b6b0791571a957b174d3fcc90ae6c48da4d10aef
juancabana <juand.cabanat@unac.edu.co>
2022-05-06 11:36:10 -0500

    Y

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import Header from "../../coponents/Header/Header";
+import ContentHome from "../../coponents/ContentHome/ContentHome";
 
 class Home extends Component {
   render() {
     return(
         <div>
             <Header/>
+            <ContentHome/>
+
         </div>
     );
   }
---
+++ src/coponents/ContentHome/ContentHome.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class ContentHome extends Component{
+    render(){
+        return(
+            <>
+            <h3>
+                Hi, I am Juan David Cabana Trejos
+            </h3>
+            </>
+        );
+    }
+
+}
+
+export default ContentHome;
