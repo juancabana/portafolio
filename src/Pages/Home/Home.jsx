5f6f69f300fc74712500c01185c3be0789e217ae
juancabana <juand.cabanat@unac.edu.co>
2022-05-18 11:25:35 -0500

    Commit retoques

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Header from "../../components/Header/Header";
 import ContentHome from "../../components/ContentHome/ContentHome";
 import Footer from "../../components/Footer/Footer";
 
+
 class Home extends Component {
   render() {
     return(
