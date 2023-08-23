3e623f6de0fc316f83d3a728f43aae60d32de058
juancabana <juand.cabanat@unac.edu.co>
2023-08-23 17:07:01 -0500

    Update info

--- src/index.js
+++ src/index.js
@@ -14,11 +14,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
-        // {
-        //   "time": "MARCH - MAY 2022",
-        //   "position": "Frontend Developer",
-        //   "company": "EDSoft",
-        //   "description": "Front-End Developer on an exciting student app project, where we use React to create a dynamic and friendly user experience. The idea of ​​this page is to offer students a modern platform to access educational resources and enhance their academic experience.",
-        //   "tecnologies": ["React", "JavaScript", "HTML", "CSS", "GraphQL"]
-        // },
